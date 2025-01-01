0424725e4bfd627d544b90fdfe2ba0f39afd4e37
povrajanapen <povrajanapen@gmail.com>
2025-01-02 02:55:32 +0700

    stage changes

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -14,6 +14,13 @@ resource "aws_security_group" "todo_sg" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # SSH from your IP
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
